baf2d395feaef11c7120f55b121175072373eb1f
Kritikasahota <sahotakritika029@gmail.com>
2026-01-28 22:58:44 +0530

    concept of conditional statements

---
+++ 05_Conditional Statements/01_IfStatement.js
@@ -0,0 +1,16 @@
+// let age = 25;
+// if( age >= 18)
+// {
+//     console.log("you can vote");
+// }
+
+let mode = "dark";
+let color;
+if( mode === "dark"){
+    color = "black";
+}
+
+if( mode === "light"){
+    color = "white"
+}
+console.log(color);
